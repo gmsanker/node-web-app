3ad03bc9797167ead5d4da61f8f25fc7963ba36e
gmsanker <gmsanker@gmail.com>
2017-04-21 14:59:20 -0400

    deleted the get/post code

--- server.js
+++ server.js
@@ -11,30 +11,5 @@ app.get('/', function (req, res) {
   res.send('Hello world\n');
 });
 
-//2nd App
-var request = require('request');
-
-// Set the headers
-var headers = {
-    'User-Agent':       'Super Agent/0.0.1',
-    'Content-Type':     'application/x-www-form-urlencoded'
-}
-
-// Configure the request
-var options = {
-    url: 'http://samwize.com',
-    method: 'GET',
-    headers: headers,
-    qs: {'key1': 'xxx', 'key2': 'yyy'}
-}
-
-// Start the request
-request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        // Print out the response body
-        console.log(body)
-    }
-})
-
 app.listen(PORT);
 console.log('Running on http://127.0.0.1:' + PORT);
