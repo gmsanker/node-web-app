fd52d6e74fef231328fd8c7cf0296f718e3a7cfc
gmsanker <gmsanker@gmail.com>
2017-05-10 14:29:06 -0400

    commented out request line

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-#const request = require('request');
 
 // Constants
 const PORT = 8080;
@@ -12,6 +11,5 @@ app.get('/', function (req, res) {
   res.send('Hello world\n');
 });
 
-
 app.listen(PORT);
-console.log('Running on http://127.0.0.1:' + PORT);
+console.log('Running on http://localhost:' + PORT);
